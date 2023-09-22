2f82ef95cd457ddf1eb41c0d645039dc2adfe3dd
Ramsnes <115258511+Ramsnes@users.noreply.github.com>
2023-09-22 02:43:30 +0200

    success hamb menu

--- js/hamburger.js
+++ js/hamburger.js
@@ -2,5 +2,5 @@ const toggleButton = document.getElementsByClassName("toggle-button")[0];
 const navbarlinks = document.getElementsByClassName("navbar-links")[0];
 
 toggleButton.addEventListener("click", () => {
-  navbarlinks.classList.toggle("active");
+  navbarlinks.classList.toggle("active"); //creates class active, which is styled
 });
