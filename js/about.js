9b144adb1aac3fc8474073a24e13ccb2b582b071
Ramsnes <115258511+Ramsnes@users.noreply.github.com>
2023-09-28 00:53:18 +0200

    finished mq desktop about

--- js/about.js
+++ js/about.js
@@ -1,2 +1,12 @@
-let sectionOne = document.querySelector(".section1");
-let sectionTwo = document.querySelector(".section2");
+// Select all timeline events
+const timelineEvents = document.querySelectorAll(".timeline-event");
+
+// Add event listeners to each event header
+timelineEvents.forEach((event) => {
+  const eventHeader = event.querySelector("h3");
+
+  eventHeader.addEventListener("click", function () {
+    // Toggle the 'active' class to show/hide the content
+    event.classList.toggle("active");
+  });
+});
