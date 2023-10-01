267eea9dff7478c98019da07e232955dec00c0a0
Ramsnes <115258511+Ramsnes@users.noreply.github.com>
2023-10-01 17:46:53 +0200

    successfully reworked the contact form, not styled yet

--- js/contact.js
+++ js/contact.js
@@ -5,42 +5,105 @@ const message = document.getElementById("message");
 const form = document.getElementById("form");
 const errorElement = document.getElementById("error");
 
+// Error divs
+let nameErrorField = document.querySelector(".nameError");
+let emailError = document.querySelector(".emailError");
+let subjectError = document.querySelector(".subjectError");
+let messageBoxError = document.querySelector(".messageBoxError");
+
 form.addEventListener("submit", (e) => {
   let messages = [];
 
+  // Name
   if (nameBox.value === "" || nameBox.value == null) {
-    messages.push("A name is required with more than 5 characters long");
-  }
-  if (email.value === "" || email.value == null) {
-    messages.push(
-      "An email address is required. Please include the symbol '@'"
-    );
-  }
-  if (subjectBox.value === "" || subjectBox.value == null) {
-    messages.push("Please provide a subject");
-  }
-  if (message.value === "" || message.value == null) {
-    messages.push("A message is required");
+    const errorMessage =
+      "A name is required with more than 5 characters long. ";
+    messages.push(errorMessage);
+
+    let combinedErrorMessage = errorMessage;
+    if (nameBox.value.length < 5) {
+      const errorMessage2 = "Full name must exceed 5 characters. ";
+      messages.push(errorMessage2);
+      combinedErrorMessage += " " + errorMessage2;
+    }
+    nameErrorField.innerText = combinedErrorMessage;
   }
 
   if (nameBox.value.length < 5) {
-    messages.push("Full name must exceed 5 characters");
+    const errorMessage2 = "Full name must exceed 5 characters";
+    messages.push(errorMessage2);
+  }
+
+  // Email
+  if (email.value === "" || email.value == null) {
+    const errorMessage = "An email address is required.";
+    messages.push(errorMessage);
+    emailError.innerText = errorMessage; // Set the error message for email
+  }
+
+  // Subject
+  if (subjectBox.value === "" || subjectBox.value == null) {
+    const errorMessage = "Please provide a subject";
+    messages.push(errorMessage);
+    subjectError.innerText = errorMessage; // Set the error message for subject
+  }
+
+  // Message
+  if (message.value === "" || message.value == null) {
+    const errorMessage = "A message is required";
+    messages.push(errorMessage);
+    messageBoxError.innerText = errorMessage; // Set the error message for message
   }
 
   if (message.value.length <= 12) {
-    messages.push("Message must be of 12 or more characters");
+    const errorMessage = "Message must be of 12 or more characters";
+    messages.push(errorMessage);
+    messageBoxError.innerText = errorMessage; // Set the error message for message length
   }
 
   if (message.value.length >= 100) {
-    messages.push("Message must be less than 100 or more characters");
+    const errorMessage = "Message must be less than 100 or more characters";
+    messages.push(errorMessage);
+    messageBoxError.innerText = errorMessage; // Set the error message for message length
   }
 
   if (subjectBox.value.length <= 6) {
-    messages.push("Subject must be of 12 or more characters");
+    const errorMessage = "Subject must be of 6 or more characters";
+    messages.push(errorMessage);
+    subjectError.innerText = errorMessage; // Set the error message for subject length
   }
 
   if (messages.length > 0) {
-    e.preventDefault(); //prevents page reload
-    errorElement.innerText = messages.join(" ...  "); //all error messages joined, each separated by a comma
+    e.preventDefault(); // Prevents page reload
+    errorElement.innerText = messages.join(" ...  "); // All error messages joined, each separated by a comma
+  }
+});
+
+// Clear error messages when input changes
+nameBox.addEventListener("input", () => {
+  if (nameBox.value.length >= 5) {
+    nameErrorField.innerText = "";
+  }
+});
+
+email.addEventListener("input", () => {
+  if (email.value !== "An email address is required.") {
+    emailError.innerText = "";
+  }
+});
+
+subjectBox.addEventListener("input", () => {
+  if (subjectBox.value !== "Please provide a subject") {
+    subjectError.innerText = "";
+  }
+});
+
+message.addEventListener("input", () => {
+  if (
+    message.value !== "A message is required" &&
+    message.value !== "Message must be of 12 or more characters" &&
+    message.value !== "Message must be less than 100 or more characters"
+  ) {
+    messageBoxError.innerText = "";
   }
 });
